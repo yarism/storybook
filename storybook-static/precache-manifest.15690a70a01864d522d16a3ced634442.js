5c30f5b5171925dc13ab383264f8403af7858123
Joakim Johansson <johansson.joakim@gmail.com>
2019-05-10 15:18:01 +0200

    fix

---
+++ storybook-static/precache-manifest.15690a70a01864d522d16a3ced634442.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d62f7c8766419d08a2257b0b88263f1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3882a0e3c3b6961b3733.bundle.js"
+  },
+  {
+    "url": "runtime~main.3882a0e3c3b6961b3733.bundle.js"
+  },
+  {
+    "url": "vendors~main.3882a0e3c3b6961b3733.bundle.js"
+  }
+]);
